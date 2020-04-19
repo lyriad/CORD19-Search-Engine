dbf32bfd946c302b4f0d803d2ce43ea99f01e105
lyriad <manueleduardo0320@gmail.com>
2020-04-19 00:01:10 -0400

    added search functionality

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 import numpy as np
 import pandas as pd
@@ -8,14 +8,14 @@ import math
 import pickle
 import json
 
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.feature_extraction.text import CountVectorizer
-# from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# df = pd.read_csv('data/articles_dataset.csv')
-# df.head()
-# vectorizer = pickle.load(open("data/vectorizer.pickle", "rb"))
-# train = pickle.load(open("data/train.pickle", "rb"))
+df = pd.read_csv('data/articles_dataset.csv')
+df.head()
+vectorizer = pickle.load(open("data/vectorizer.pickle", "rb"))
+train = pickle.load(open("data/train.pickle", "rb"))
 
 load_dotenv()
 
@@ -27,27 +27,24 @@ mongo2 = PyMongo(app, uri = os.getenv("MONGO_URI_2"))
 
 @app.route('/')
 def index():
-    return render_template('search.html', indexes = [1, 2, 4, 3, 5, 9, 8, 0])
+    query = request.args['query'] if 'query' in request.args else None
+    articles = []
+    if query is not None:
+        query_vec = vectorizer.transform([query])
+        results = cosine_similarity(train,query_vec).reshape((-1,))
+
+        for i in results.argsort()[-10:][::-1]:
+            article = {}
+            if( results[i] > 0.2 ):
+                article['id'] = df.iloc[i,0]
+                article['title'] = df.iloc[i,1]
+                articles.append(article)
+
+    return render_template('search.html', articles = articles)
 
 @app.route('/test')
 def testdb():
     return  json.dumps({'test': [x for x in mongo2.db.articles.find({'paper_id': 'be5bddac1b08e3f6c757bdbd6564255e6ee96f4a'},{'_id':0})]})
-
-# @app.route('/search', methods=['GET'])
-# def search():
-#     query = 'coronavirus'
-#     show_results = []
-#     query_vec = vectorizer.transform([query])
-#     results = cosine_similarity(train,query_vec).reshape((-1,))
-
-#     for i in results.argsort()[-10:][::-1]:
-#         article = {}
-#         if( results[i] > 0.2 ):
-#             article['id'] = df.iloc[i,0]
-#             article['title'] = df.iloc[i,1]
-#             show_results.append(article)
-    
-#     return json.dumps({'results': show_results})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
